690f046fb7df70804aa0909ad2fe42e2ae705ad2
nicolai1233 <nicolai1233@users.noreply.github.com>
2018-04-18 13:32:07 +0200

    GUI

--- src/javafxapplication2/FXMLDocumentController.java
+++ src/javafxapplication2/FXMLDocumentController.java
@@ -10,7 +10,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -18,10 +21,16 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private AnchorPane loginScreenPane;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private Button loginButton;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -31,5 +40,10 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void loginButtonClick(ActionEvent event) {
+        
+    }
     
 }
